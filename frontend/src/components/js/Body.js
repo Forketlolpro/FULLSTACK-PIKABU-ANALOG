1e87f2fe1c3689320f55aac4c1c01f495ad120b4
Forketlolpro <ilyacheburov@yandex.ru>
2019-10-20 14:09:16 +0300

    wip

--- frontend/src/components/js/Body.js
+++ frontend/src/components/js/Body.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router,Route,Link,Switch,Redirect} from "react-router-dom";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import styles from '../scss/Body.scss';
 
 
--- frontend/src/components/js/LoginForm.js
+++ frontend/src/components/js/LoginForm.js
@@ -3,45 +3,44 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styles from '../scss/LoginForm.scss';
 
-class LoginForm extends React.Component {
-    render() {
-        return (
-            <Formik
-                initialValues={{
-                    email: '',
-                    password: ''
-                }}
-                validationSchema={Yup.object().shape({
-                    email: Yup.string()
-                        .email('Email is invalid')
-                        .required('Email is required'),
-                    password: Yup.string()
-                        .min(6, 'Password must be at least 6 characters')
-                        .required('Password is required')
-                })}
-                onSubmit={fields => {
-                    console.log(JSON.stringify(fields, null, 4))
-                }}
-                render={({ errors, status, touched }) => (
-                    <Form>
-                        <div className={styles.FormGroup}>
-                            <label htmlFor="email">Email</label>
-                            <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
-                            <ErrorMessage name="email" component="div" className={styles.InvalidFeedback} />
-                        </div>
-                        <div className={styles.FormGroup}>
-                            <label htmlFor="password">Password</label>
-                            <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
-                            <ErrorMessage name="password" component="div" className={styles.InvalidFeedback} />
-                        </div>
-                        <div className={styles.FormGroup}>
-                            <button type="submit" className={styles.Button}>Login</button>
-                        </div>
-                    </Form>
-                )}
-            />
-        )
-    }
+const LoginForm  = (props) => {
+    return (
+        <Formik
+            initialValues={{
+                email: '',
+                password: ''
+            }}
+            validationSchema={Yup.object().shape({
+                email: Yup.string()
+                    .email('Email is invalid')
+                    .required('Email is required'),
+                password: Yup.string()
+                    .min(6, 'Password must be at least 6 characters')
+                    .required('Password is required')
+            })}
+            onSubmit={fields => {
+                console.log(JSON.stringify(fields, null, 4))
+            }}
+            render={({ errors, status, touched }) => (
+                <Form className={styles.LoginForm}>
+                    <div className={styles.FormGroup}>
+                        <label htmlFor="email">Email</label>
+                        <Field name="email" type="text" className={styles.FormInput + ' form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
+                        <ErrorMessage name="email" component="div" className={styles.InvalidFeedback} />
+                    </div>
+                    <div className={styles.FormGroup}>
+                        <label htmlFor="password">Password</label>
+                        <Field name="password" type="password" className={styles.FormInput + ' form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
+                        <ErrorMessage name="password" component="div" className={styles.InvalidFeedback} />
+                    </div>
+                    <div className={styles.FormGroup}>
+                        <a href="/registration">Зарегестрироваться</a>
+                        <button type="submit" className={styles.Button}>Login</button>
+                    </div>
+                </Form>
+            )}
+        />
+    )
 }
 
 export { LoginForm }; 
